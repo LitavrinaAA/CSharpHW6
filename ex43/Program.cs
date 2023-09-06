6e47742850326d34f090c1dba6d3b4e14d4ae445
LitavrinaAA <a.a.litavrina@google.com>
2023-09-06 17:38:31 +0300

    strights are crossing? Where?! return X Y ver1.2

--- ex43/Program.cs
+++ ex43/Program.cs
@@ -8,14 +8,16 @@
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 int InputValue(string mess){
-   Console.WriteLine("введите значение b1");
+   Console.WriteLine(mess);
    return int.Parse(Console.ReadLine()!); 
 }
 double GetX(int b1, int b2, int k1, int k2){
-    return (b2 - b1)/(k1 - k2);
+    double res = (double)(b2 - b1)/(k1 - k2);  
+    return res;
 }
 
-double GetY(int b1, int b2, int k1, int k2, double x){
+double GetY(int b2, int k2, double x){
+    
     return k2 * x + b2;
 }
 int b1 = InputValue("введите значение b1");
@@ -25,7 +27,10 @@ int b2 = InputValue("введите значение b2");
 int k2 = InputValue("введите число k2");
 
 
-double x = GetX(b1:b1, b2:b2, k1:k1, k2:k2);//(-b2 + b1)/(-k1 + k2);
-double y = GetY(b1:b1, b2:b2, k1:k1, k2:k2, x:x);//k2 * x + b2;
+double x = GetX(b1, b2, k1, k2);//(-b2 + b1)/(-k1 + k2);
+double y = GetY(b2, k2, x);//k2 * x + b2;
+
+System.Console.WriteLine(x);
+System.Console.WriteLine(y);
 
 Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
